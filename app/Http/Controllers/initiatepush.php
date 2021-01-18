7de881f89a524269d3552ca7ef4a7d9e268bde44
devolper102 <muhammad_sikander@outlook.com>
2021-01-18 12:16:34 +0500

    mpesa

--- app/Http/Controllers/initiatepush.php
+++ app/Http/Controllers/initiatepush.php
@@ -4,21 +4,81 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
+
 use Illuminate\Support\Facades\Log;
 
 class initiatepush extends Controller
 {
         public function pay(Request $request){
-            $Amount = $request->input('amount');
-            $phoneNumber = $request->input('phonenumber');
+            $Amount =$request->input('amount');
+            
+            $phoneNumber = "254".substr($request['phonenumber'], -9);
        
             $CallBackURL = 'https://frecourses.com/ticket/public/callback';
         
-        	
+             Log::error('INITIATION PHONE RECEIVED: '.$phoneNumber);
+             
+             if($Amount != 0){
         
-        $mpesa= new \Safaricom\Mpesa\Mpesa();
+                $mpesa= new \Safaricom\Mpesa\Mpesa();
+                
+                $stkPushSimulation=$mpesa->STKPushSimulation(174379, 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919', 'CustomerPayBillOnline', $Amount, $phoneNumber, 174379, $phoneNumber, $CallBackURL, 'lozadasuplies', 'lozada', 'Payment');
         
-        $stkPushSimulation=$mpesa->STKPushSimulation(174379, 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919', 'CustomerPayBillOnline', $Amount, $phoneNumber, 174379, $phoneNumber, $CallBackURL, 'lozadasuppies', 'lozada', 'Payment');
-        
+                
+               if (!empty($stkPushSimulation)){
+                
+               $state = json_decode($stkPushSimulation);
+              
+             if (property_exists($state,"ResponseCode")){
+               
+                        $ResponseCode = $state->ResponseCode;
+                        
+                        $CustomerMessage = $state->CustomerMessage;
+                        
+                        if ($ResponseCode == '0'){
+                        
+                                $MerchantRequestID = $state->MerchantRequestID;
+                                
+                                $CheckoutRequestID = $state->CheckoutRequestID;
+                                     
+                                     DB::insert('INSERT INTO payments
+                                             ( 
+                                             MerchantRequestID,
+                                             CheckoutRequestID
+                                             
+                                             )   values (?, ?)',
+                                             [$MerchantRequestID,
+                                              $CheckoutRequestID
+                                            ] );
+                                     return view('waiting', ['CheckoutRequestID' => $CheckoutRequestID,'CustomerMessage' =>$CustomerMessage,'complete'=>false]);
+                            }
+                            else
+                            {
+                       
+                                    return view('waiting', ['CheckoutRequestID' =>0, 'CustomerMessage' =>$CustomerMessage,'complete'=>true]);
+                            }
+               }
+               elseif(array_key_exists("errorCode",$state)){
+                
+                    if(($state->errorCode) == "500.001.1001"){
+                        $CustomerMessage = "Looks like you provided an invalid phone number";
+                        return view('waiting', ['CheckoutRequestID' =>0,'CustomerMessage' =>$CustomerMessage,'complete'=>true]);
+                    }else{
+                         $CustomerMessage = "Bad payment request, please contact support";
+                        return view('waiting', ['CheckoutRequestID' =>0,'CustomerMessage' =>$CustomerMessage,'complete'=>true]);
+                    }
+               
+               }else{
+                 $CustomerMessage = "Bad payment request, please contact support";
+                        return view('waiting', ['CheckoutRequestID' =>0,'CustomerMessage' =>$CustomerMessage,'complete'=>true]);
+               }
+               }
+     
+        }else
+        {
+        echo "Done"; 
+        }
+    
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -65,4 +65,15 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
 
 Route::post('/requestpay','initiatepush@pay')->name('requestpay');
 
+
+Route::get('/result', function () {
+    return View::make('result', ['receipt' => 'HDYDNSKSK','amount' => 10]);
+});
+
+Route::get('/result_fail', function () {
+    return View::make('result', ['reason' => 'Wrong pin']);
+});
+
 Route::post('/callback','confirmcallback@storeResults');
+
+Route::get('/check/{CheckoutRequestID}','confirmcallback@check');
